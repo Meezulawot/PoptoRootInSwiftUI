c435e7f229da03473166667471a8dd89660637b1
Meezu Lawot <meezu.lawot@swifttech.com.np>
2023-09-24 13:13:44 +0545

    Initial Commit

---
+++ NavigationSwiftUI/NavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSwiftUIApp.swift
+//  NavigationSwiftUI
+//
+//  Created by Meezu Lawot on 24/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
